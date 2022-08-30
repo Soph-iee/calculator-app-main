ceaae3764653a08a34fad41d0e762e3189575e82
soph-iee <xophieadrian@gmail.com>
2022-08-30 17:14:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+"use strict";
+
+// defining the varibles
+const output = document.querySelector(".cal-head");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const deleteBtn = document.querySelector(".delete-btn");
+const resetBtn = document.querySelector(".reset-btn");
+const resultBtn = document.querySelector(".equal-btn");
+const redTheme = document.querySelector(".red");
+const themeTwo = document.querySelector(".cyan");
+const greenTheme = document.querySelector(".green");
+/// functionalities
+// let num = 3455555;
+// num = num.toLocaleString("fr-FR");
+// console.log(num);
+
+// num = num.toString().replace(/\B(?=(?:\d{3})+)$/g, ",");
+// console.log(num);
+
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", function () {
+    output.value += numbers[i].value;
+  });
+}
+for (let i = 0; i < operators.length; i++) {
+  operators[i].addEventListener("click", function () {
+    if (output.value !== "" && !output.value.includes(operators[i].value)) {
+      output.value += operators[i].value;
+    }
+  });
+}
+
+resultBtn.addEventListener("click", function () {
+  let result = eval(output.value);
+  output.value = result;
+});
+resetBtn.addEventListener("click", function () {
+  output.value = "";
+});
+
+redTheme.addEventListener("click", function () {
+  document.querySelector("body").classList.remove("theme-two");
+  document.querySelector("body").classList.remove("theme-three");
+});
+themeTwo.addEventListener("click", function () {
+  document.querySelector("body").classList.add("theme-two");
+  document.querySelector("body").classList.remove("theme-three");
+});
+greenTheme.addEventListener("click", function () {
+  document.querySelector("body").classList.add("theme-three");
+  document.querySelector("body").classList.remove("theme-two");
+});
+deleteBtn.addEventListener("click", function () {
+  let string = output.value.toString();
+  output.value = string.substr(0, string.length - 1);
+  console.log("ckicked");
+});
+
+// testing functionalities with a small project
+// const numberField = document.querySelector(".number-field");
+// const calculate = document.querySelector(".btn");
+// const result = document.querySelector(".result");
+
+// calculate.addEventListener("click", function () {
+//   const number = document.querySelector(".number-field").value;
+//   eval(number);
+//   result.textContent = eval(number);
+//   numberField.value = result.textContent;
+//   console.log(result.textContent);
+//
